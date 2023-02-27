a3bf2ec341f4abf428c803799c71fce168a4325e
ChipCruncher72 <117965593+ChipCruncher72@users.noreply.github.com>
2023-02-27 18:03:51 +1030

    Fixed the Commands receiving errors

--- src/main/java/com/fonkyclubhouse/bot/listeners/ServerCommands.java
+++ src/main/java/com/fonkyclubhouse/bot/listeners/ServerCommands.java
@@ -10,7 +10,9 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.zone.ZoneRulesException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class ServerCommands extends ListenerAdapter {
@@ -19,7 +21,7 @@ public class ServerCommands extends ListenerAdapter {
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
 		String command = event.getName();
 		Random random = new Random();
-		// I am aware of switch statements. I'm just too lazy to use them.
+		// I am aware of switch statements. I'm just too lazy to refactor the code.
 		if (command.equals("info")) {
 			event.reply("Bot was created on the 26th of February 2023\n\nCreated by ChipCruncher72#8376\n\nWith the JDA (https://github.com/DV8FromTheWorld/JDA)").setEphemeral(true).queue();
 		} else if (command.equals("fun-fact")) {
@@ -33,17 +35,24 @@ public class ServerCommands extends ListenerAdapter {
 			};
 			event.reply(fact).queue();
 		} else if (command.equals("repeat")) {
-			if (event.getOption("bold").getAsBoolean()) {
-				event.reply(event.getMember().getAsMention() + " sent **" + event.getOption("say").getAsString() + "**").queue();
+			if (event.getOption("bold") != null) {
+				if (event.getOption("bold").getAsBoolean()) {
+					event.reply(event.getMember().getAsMention() + " sent **" + event.getOption("say").getAsString() + "**").queue();
+				} else {
+					event.reply(event.getMember().getAsMention() + " sent " + event.getOption("say").getAsString()).queue();
+				}
 			} else {
-				event.reply(event.getMember().getAsMention() + " sent " + event.getOption("say").getAsString()).queue();
+				event.reply(event.getMember().getAsMention() + " sent **" + event.getOption("say").getAsString() + "**").queue();
 			}
 		} else if (command.equals("time")) {
 			if (event.getOption("timezone").getAsString().equals("help")) {
 				event.reply("Here are all the available timezones.\nhttps://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/").setEphemeral(true).queue();
 			} else {
 				String timezone = event.getOption("timezone").getAsString();
-				ZoneId zoneId = ZoneId.of(timezone);
+				ZoneId zoneId = ZoneId.of("UTC");
+				try {
+					zoneId = ZoneId.of(timezone);
+				} catch (ZoneRulesException ignore) {}
 				String fullTime = String.valueOf(LocalTime.now(zoneId));
 				String neededTime = fullTime.substring(0, 8);
 				event.reply("The current " + timezone + " time is: **" + LocalDate.now(zoneId) + " " + neededTime + "**").queue();
