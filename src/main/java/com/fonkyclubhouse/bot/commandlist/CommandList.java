ef09266e29e76a70f2c12c739d51c5fbe895ac9b
ChipCruncher72 <117965593+ChipCruncher72@users.noreply.github.com>
2023-02-27 18:04:27 +1030

    Fixed the commands receiving errors

--- src/main/java/com/fonkyclubhouse/bot/commandlist/CommandList.java
+++ src/main/java/com/fonkyclubhouse/bot/commandlist/CommandList.java
@@ -12,8 +12,8 @@ public class CommandList {
 		List<CommandData> cmds = new ArrayList<>();
 		cmds.add(Commands.slash("info", "Information about the bot!"));
 		cmds.add(Commands.slash("fun-fact", "Generate a random fun fact!"));
-		cmds.add(Commands.slash("repeat", "Will repeat the string provided.").addOption(OptionType.STRING, "say", "repeat").addOption(OptionType.BOOLEAN, "bold", "If it will repeat your message boldly"));
-		cmds.add(Commands.slash("time", "time in provided timezone. type 'help' for help").addOption(OptionType.STRING, "timezone", "said timezone"));
+		cmds.add(Commands.slash("repeat", "Will repeat the string provided.").addOption(OptionType.STRING, "say", "repeat", true).addOption(OptionType.BOOLEAN, "bold", "If it will repeat your message boldly"));
+		cmds.add(Commands.slash("time", "time in provided timezone. type 'help' for help").addOption(OptionType.STRING, "timezone", "said timezone", true));
 		return cmds;
 	}
 }
