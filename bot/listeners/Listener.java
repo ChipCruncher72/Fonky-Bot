fb158ea6ff8143c5ee1c716103cd71ead50f496c
ChipCruncher72 <117965593+ChipCruncher72@users.noreply.github.com>
2023-03-14 11:42:22 +1030

    Update Listener.java

--- bot/listeners/Listener.java
+++ bot/listeners/Listener.java
@@ -8,8 +8,6 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.Objects;
-
 public class Listener extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
@@ -37,7 +35,7 @@ public class Listener extends ListenerAdapter {
 	@Override
 	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 		JDA jda = event.getJDA();
-		event.getGuild().addRoleToMember(event.getMember(), Objects.requireNonNull(jda.getRoleById("1079200021502775387"))).queue();
+		event.getGuild().addRoleToMember(event.getMember(), jda.getRoleById("1079200021502775387")).queue();
 		event.getGuild().getChannelById(TextChannel.class, "1079233593609560094").sendMessage("Welcome to the clubhouse " + event.getMember().getAsMention() + "!").queue();
 	}
 }
